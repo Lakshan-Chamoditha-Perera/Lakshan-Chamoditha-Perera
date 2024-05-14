89524e5ac11dc90ab978103622be700ffea3bb80
Lakshan Chamoditha <chamodithap@ceyentra.com>
2024-05-14 19:07:23 +0530

    dev initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from './components/header/Header';
+
+function App()
+{
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,66 @@
+import React, {useState} from "react";
+import "./header.css";
+
+const Header = () => {
+    const [Toggle, setToggle] = useState(false);
+    return (
+        <header className="header">
+            <nav className="nav container">
+                <a href="index.html" className="nav__logo">
+                    Logo
+                </a>
+                <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
+                    <ul className="nav__list grid">
+                        <li className="nav__item">
+                            <a href="#home" className="nav__link active-link">
+                                <i className="uil uil-estate nav__icon"></i>
+                                Home
+                            </a>
+                        </li>
+
+                        <li className="nav__item">
+                            <a href="#about" className="nav__link">
+                                <i className="uil uil-user nav__icon"></i>
+                                About
+                            </a>
+                        </li>
+                        <li className="nav__item">
+                            <a href="#skills" className="nav__link">
+                                <i className="uil uil-file-alt nav__icon"></i>
+                                Skills
+                            </a>
+                        </li>
+                        <li className="nav__item">
+                            <a href="#service" className="nav__link">
+                                <i className="uil uil-briefcase-alt nav__icon"></i>
+                                services
+                            </a>
+                        </li>
+                        <li className="nav__item">
+                            <a href="#portfolio" className="nav__link">
+                                <i className="uil uil-scenery nav__icon"></i>
+                                Portfolio
+                            </a>
+                        </li>
+
+                        <li className="nav__item">
+                            <a href="#contact" className="nav__link">
+                                <i className="uil uil-message nav__icon"></i>
+                                Contact
+                            </a>
+                        </li>
+                    </ul>
+
+                    <i className="uil uil-times nav__close" onClick={() => setToggle(!Toggle)}></i>
+                </div>
+
+                <div className="nav__toggle" onClick={() => setToggle(!Toggle)}>
+                    <i className="uil uil-apps"></i>
+
+                </div>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
