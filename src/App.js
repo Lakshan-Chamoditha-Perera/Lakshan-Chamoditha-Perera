aa16ba27af23ce5bc7e555f4f6bedb5e4dfa79bd
Lakshan Chamoditha Perera <perera.alc2000@gmail.com>
2024-06-19 22:01:00 +0530

    about section added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/header/Header';
 import Home from "./components/home/Home";
+import About from "./components/about/About";
 
 function App() {
     return (
         <>
             <Header/>
             <Home/>
+            <About/>
         </>
     );
 }
